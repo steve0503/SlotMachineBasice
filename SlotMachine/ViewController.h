5ed8681368439e5fd873b43e66279539d75ba002
해용 이 <neo0503@haeyong-ui-MacBook-Pro.local>
2014-01-07 23:45:17 +0900

    Initial Commit

---
+++ SlotMachine/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SlotMachine
+//
+//  Created by 해용 이 on 2014. 1. 7..
+//  Copyright (c) 2014년 해용 이. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SlotMachine/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SlotMachine
+//
+//  Created by 해용 이 on 2014. 1. 7..
+//  Copyright (c) 2014년 해용 이. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
