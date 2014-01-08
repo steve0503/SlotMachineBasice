db3c2f3fc2dc8d325b8a375b0dcac73193e453e5
steve0503 <leehr76@gmail.com>
2014-01-08 09:42:59 +0900

    SlotMachine_Basic
    Done

--- SlotMachine/ViewController.m
+++ SlotMachine/ViewController.m
@@ -6,13 +6,76 @@
 //  Copyright (c) 2014년 해용 이. All rights reserved.
 //
 
+#define MAX_NUM 100
+
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UIPickerViewDelegate, UIPickerViewDataSource>
+@property (weak, nonatomic) IBOutlet UIPickerView *picker;
 
 @end
 
 @implementation ViewController
+
+
+-(IBAction)startMachine:(id)sender{
+    
+    int r1 = arc4random()%MAX_NUM;
+    
+    [self.picker selectRow:r1 inComponent:0 animated:YES];
+    
+    
+    int r2 = arc4random()%MAX_NUM;
+    
+    [self.picker selectRow:r2 inComponent:1 animated:YES];
+    
+    int r3 = arc4random()%MAX_NUM;
+    
+    [self.picker selectRow:r3 inComponent:2 animated:YES];
+}
+
+-(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{
+    
+    return 3;
+    
+}
+
+-(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component{
+    
+    
+    return MAX_NUM;
+}
+
+-(CGFloat)pickerView:(UIPickerView *)pickerView rowHeightForComponent:(NSInteger)component{
+    
+    return 64;
+    
+}
+
+-(UIView *)pickerView:(UIPickerView *)pickerView viewForRow:(NSInteger)row forComponent:(NSInteger)component reusingView:(UIView *)view{
+    
+    NSString *imagePath = [NSString stringWithFormat:@"flag%ld.jpeg",row%10];
+    
+    UIImage *image = [UIImage imageNamed:imagePath];
+    
+    UIImageView *imageView;
+    
+    if (nil == view) {
+        imageView = [[UIImageView alloc] initWithImage:image];
+        imageView.frame = CGRectMake(0, 0, 100, 60);
+        
+    }
+    else{
+        
+        imageView = (UIImageView *)view;
+        imageView.image = image;
+        
+    }
+    
+    return imageView;
+    
+    
+}
 
 - (void)viewDidLoad
 {
